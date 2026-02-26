0a2239781eef2c586e28f70882647af1e0fedb65
Ezandora <ezandora@gmail.com>
2026-02-26 10:41:19 -0600

    Wiki links on numberology
    As suggested by Arlannah.

--- kol/numberology.js
+++ kol/numberology.js
@@ -2,28 +2,28 @@
 
 function calculateNumberologyValues(ascensions, level, spleen_used, adventures, moon_sign_id)
 {
-	var b = spleen_used + level;
-	var c = (ascensions + moon_sign_id) * b + adventures;
+	let b = spleen_used + level;
+	let c = (ascensions + moon_sign_id) * b + adventures;
 	
-	var solutions = [];
-	var closest_point = [];
+	let solutions = [];
+	let closest_point = [];
 	
 	
-	for (var x = 0; x <= 99; x++)
+	for (let x = 0; x <= 99; x++)
 	{
 		closest_point[x] = 99;
 	}
 	
-	for (var x = 0; x <= 99; x++)
+	for (let x = 0; x <= 99; x++)
 	{
-		var v = x * b + c;
-		var last_two_digits = v % 100;
+		let v = x * b + c;
+		let last_two_digits = v % 100;
 		
 		solutions[last_two_digits] = x;
 		
-		for (var x2 = 0; x2 <= 99; x2++)
+		for (let x2 = 0; x2 <= 99; x2++)
 		{
-			var delta = x2 - last_two_digits;
+			let delta = x2 - last_two_digits;
 			if (delta <= 0)
 				closest_point[x2] = Math.min(closest_point[x2], -delta);
 			else
@@ -35,7 +35,7 @@ function calculateNumberologyValues(ascensions, level, spleen_used, adventures, 
 		}
 	}
 	
-	var result = {};
+	let result = {};
 	result["solutions"] = solutions;
 	result["adventures until solution"] = closest_point;
 	return result;
@@ -43,11 +43,14 @@ function calculateNumberologyValues(ascensions, level, spleen_used, adventures, 
 
 function numberologyExplanations()
 {
-	var explain_it_all = [];
-	var nons = [24,25,26,28,29,31,32,39,41,42,46,52,53,54,55,56,59,60,61,62,64,65,67,72,73,74,76,79,80,81,82,84,85,86,91,92,94,95,96];
-	for (var i = 0; i < nons.length; i++)
+	let explain_it_all = [];
+	let nons = [24,25,26,28,29,31,32,39,41,42,46,52,53,54,55,56,59,60,61,62,64,65,67,72,73,74,76,79,80,81,82,84,85,86,91,92,94,95,96];
+	for (let i = 0; i < nons.length; i++)
 		explain_it_all[nons[i]] = "Try Again";
 	//na, na, na na na
+	
+	let bottles = "<a href=\"https://wiki.kingdomofloathing.com/Bottle_of_gin\">bottle of gin</a>, <a href=\"https://wiki.kingdomofloathing.com/Bottle_of_rum\">bottle of rum</a>, <a href=\"https://wiki.kingdomofloathing.com/Bottle_of_vodka\">bottle of vodka</a>, or <a href=\"https://wiki.kingdomofloathing.com/Bottle_of_whiskey\">bottle of whiskey</a>";
+	
 	explain_it_all[0] = "0 meat";
 	explain_it_all[1] = "seal-clubbing club";
 	explain_it_all[2] = "Sleepy (100 adventures)";
@@ -59,14 +62,14 @@ function numberologyExplanations()
 	explain_it_all[8] = "Hardly Poisoned At All (100 adventures)";
 	explain_it_all[9] = "Knob Goblin Perfume (100 adventures)";
 	explain_it_all[10] = "Steriod Boost (100 adventures)";
-	explain_it_all[11] = "+ or - drunkenness?";
+	explain_it_all[11] = "-1 to +3 drunkenness";
 	explain_it_all[12] = "Fight gnollish gearhead";
 	explain_it_all[13] = "Nothing... or is it?";
 	explain_it_all[14] = "14 moxie weeds (autosell for 1400 meat)";
 	explain_it_all[15] = "15 meat";
 	explain_it_all[16] = "[?-3] magicalness-in-a-can";
 	explain_it_all[17] = "+1 adventure";
-	explain_it_all[18] = "[bottle of rum, bottle of gin, bottle of vodka, ?]";
+	explain_it_all[18] = bottles;
 	explain_it_all[19] = "+21? moxie?";
 	explain_it_all[20] = "-19-20? mainstat?";
 	explain_it_all[21] = "+1 fight";
@@ -82,7 +85,7 @@ function numberologyExplanations()
 	explain_it_all[38] = "+24? myst?";
 	explain_it_all[40] = "40 meat";
 	explain_it_all[43] = "+26? muscle?";
-	explain_it_all[44] = "[bottle of rum, bottle of whiskey, ?]";
+	explain_it_all[44] = bottles;
 	explain_it_all[45] = "[?] magicalness-in-a-can";
 	explain_it_all[47] = "+44? moxie?";
 	explain_it_all[48] = "Fight your butt";
@@ -97,7 +100,7 @@ function numberologyExplanations()
 	explain_it_all[69] = "+3 adventures";
 	explain_it_all[70] = "+77? mainstat?";
 	explain_it_all[71] = "-70? myst?";
-	explain_it_all[75] = "[bottle of rum, bottle of vodka, bottle of gin, ?]";
+	explain_it_all[75] = bottles;
 	explain_it_all[77] = "spooky stick";
 	explain_it_all[78] = "+41? myst?";
 	explain_it_all[83] = "+50? muscle?";
@@ -108,33 +111,102 @@ function numberologyExplanations()
 	explain_it_all[93] = "93 meat";
 	explain_it_all[97] = "[?] magicalness-in-a-can";
 	explain_it_all[98] = "+58-59? myst?";
-	explain_it_all[99] = "[bottle of rum, bottle of gin, bottle of whiskey, ?]";
+	explain_it_all[99] = bottles;
 	
 	return explain_it_all;
 }
 
+function numberologyURLs()
+{
+	let urls = [];
+	urls[0] = ""; //0 meat
+	urls[1] = "https://wiki.kingdomofloathing.com/Seal-clubbing_club"; //seal-clubbing club
+	urls[2] = "https://wiki.kingdomofloathing.com/Sleepy"; //Sleepy (100 adventures)
+	urls[3] = "https://wiki.kingdomofloathing.com/Confused"; //Confused (100 adventures)
+	urls[4] = "https://wiki.kingdomofloathing.com/Embarrassed"; //Embarrassed (100 adventures)
+	urls[5] = "https://wiki.kingdomofloathing.com/Far_Out"; //Far Out (100 adventures)
+	urls[6] = "https://wiki.kingdomofloathing.com/Wings"; //Wings (100 adventures)
+	urls[7] = "https://wiki.kingdomofloathing.com/Beaten_Up"; //Beaten Up (100 adventures)
+	urls[8] = "https://wiki.kingdomofloathing.com/Hardly_Poisoned_at_All"; //Hardly Poisoned At All (100 adventures)
+	urls[9] = "https://wiki.kingdomofloathing.com/Knob_Goblin_Perfume"; //Knob Goblin Perfume (100 adventures)
+	urls[10] = "https://wiki.kingdomofloathing.com/Steroid_Boost"; //Steriod Boost (100 adventures)
+	urls[11] = ""; //+ or - drunkenness?
+	urls[12] = "https://wiki.kingdomofloathing.com/Gnollish_Gearhead"; //Fight gnollish gearhead
+	urls[13] = ""; //Nothing... or is it?
+	urls[14] = "https://wiki.kingdomofloathing.com/Moxie_weed"; //14 moxie weeds (autosell for 1400 meat)
+	urls[15] = ""; //15 meat
+	urls[16] = "https://wiki.kingdomofloathing.com/Magicalness-in-a-can"; //[?-3] magicalness-in-a-can
+	urls[17] = ""; //+1 adventure
+	urls[18] = ""; //[bottle of rum, bottle of gin, bottle of vodka, ?]
+	urls[19] = ""; //+21? moxie?
+	urls[20] = ""; //-19-20? mainstat?
+	urls[21] = ""; //+1 fight
+	urls[22] = "https://wiki.kingdomofloathing.com/Pygmy_phone_number"; //pygmy phone number
+	urls[23] = ""; //+22? muscle?
+	urls[27] = ""; //+25? moxie?
+	urls[30] = "https://wiki.kingdomofloathing.com/Ghuol"; //Fight a ghuol
+	urls[33] = ""; //[?] magicalness-in-a-can
+	urls[34] = ""; //+38-50? mainstat
+	urls[35] = ""; //-38? muscle?
+	urls[36] = ""; //+2 adventures
+	urls[37] = ""; //+3 fights
+	urls[38] = ""; //+24? myst?
+	urls[40] = ""; //40 meat
+	urls[43] = ""; //+26? muscle?
+	urls[44] = ""; //[bottle of rum, bottle of whiskey, ?]
+	urls[45] = ""; //[?] magicalness-in-a-can
+	urls[47] = ""; //+44? moxie?
+	urls[48] = "https://wiki.kingdomofloathing.com/Your_butt"; //Fight your butt
+	urls[49] = ""; //+49? mainstat
+	urls[50] = ""; //-53-57? myst?
+	urls[51] = "https://wiki.kingdomofloathing.com/War_Frat_151st_Infantryman"; //War Frat 151st Infantryman
+	urls[57] = ""; //+46? moxie?
+	urls[58] = "https://wiki.kingdomofloathing.com/Teleportitis"; //Teleportitis (10 adventures)
+	urls[63] = ""; //+36-40? muscle?
+	urls[66] = "https://wiki.kingdomofloathing.com/Magicalness-in-a-can"; //[?] magicalness-in-a-can
+	urls[68] = ""; //+33-41? myst?
+	urls[69] = ""; //+3 adventures
+	urls[70] = ""; //+77? mainstat?
+	urls[71] = ""; //-70? myst?
+	urls[75] = ""; //[bottle of rum, bottle of vodka, bottle of gin, ?]
+	urls[77] = "https://wiki.kingdomofloathing.com/Spooky_stick"; //spooky stick
+	urls[78] = ""; //+41? myst?
+	urls[83] = ""; //+50? muscle?
+	urls[87] = ""; //+40-52? moxie?
+	urls[88] = "https://wiki.kingdomofloathing.com/Magicalness-in-a-can"; //[?] magicalness-in-a-can
+	urls[89] = ""; //+89? mainstat
+	urls[90] = ""; //-97? muscle?
+	urls[93] = ""; //93 meat
+	urls[97] = "https://wiki.kingdomofloathing.com/Magicalness-in-a-can"; //[?] magicalness-in-a-can
+	urls[98] = ""; //+58-59? myst?
+	urls[99] = ""; //[bottle of rum, bottle of gin, bottle of whiskey, ?]
+	
+	return urls;
+}
+
+
 function generateIntegerPresenceMap(array)
 {
-	var result = [];
-	for (var i = 0; i < array.length; i++)
+	let result = [];
+	for (let i = 0; i < array.length; i++)
 		result[array[i]] = true;
 	return result;
 }
 
 function solveNumberology()
 {
-	var ascensions_input = document.getElementById("ascensions").value;
-	var level_input = document.getElementById("level").value;
-	var spleen_used_input = document.getElementById("spleen_used").value;
-	var adventures_left_input = document.getElementById("adventures_left").value;
-	var moon_sign_input = document.getElementById("moon_sign").value;
+	let ascensions_input = document.getElementById("ascensions").value;
+	let level_input = document.getElementById("level").value;
+	let spleen_used_input = document.getElementById("spleen_used").value;
+	let adventures_left_input = document.getElementById("adventures_left").value;
+	let moon_sign_input = document.getElementById("moon_sign").value;
 	
 	//Save inputs:
 	try
 	{
 		if (typeof localStorage !== "undefined")
 		{
-			var saved_ascensions = localStorage.getItem("ascensions");
+			let saved_ascensions = localStorage.getItem("ascensions");
 			if (saved_ascensions != ascensions_input)
 			{
 				document.getElementById("moon_sign").value = -1;
@@ -158,15 +230,16 @@ function solveNumberology()
 	}
 	
 	
-	var calculation_result = calculateNumberologyValues(parseInt(ascensions_input), parseInt(level_input), parseInt(spleen_used_input), parseInt(adventures_left_input), parseInt(moon_sign_input));
+	let calculation_result = calculateNumberologyValues(parseInt(ascensions_input), parseInt(level_input), parseInt(spleen_used_input), parseInt(adventures_left_input), parseInt(moon_sign_input));
 	
-	var solutions = calculation_result["solutions"];
-	var adventures_until_solution = calculation_result["adventures until solution"];
+	let solutions = calculation_result["solutions"];
+	let adventures_until_solution = calculation_result["adventures until solution"];
 	
 	
-	var explain_it_all = numberologyExplanations();
+	let explain_it_all = numberologyExplanations();
+	let explain_urls = numberologyURLs();
 	
-	var index_display_order_base = [];
+	let index_display_order_base = [];
 	index_display_order_base.push([69, 37, 51, 89]); //useful things
 	index_display_order_base.push([]); //default - will always be index 1, so try not to move this
 	index_display_order_base.push([17, 36]); //adventures
@@ -182,22 +255,25 @@ function solveNumberology()
 	index_display_order_base.push([16,33,45,66,88,97]); //magicalness-in-a-can
 	index_display_order_base.push([13]); //ooOoOoOo
 	
-	var index_display_order = [];
-	for (var i = 0; i < index_display_order_base.length; i++)
+	let index_display_order = [];
+	for (let i = 0; i < index_display_order_base.length; i++)
 	{
 		index_display_order[i] = generateIntegerPresenceMap(index_display_order_base[i]);
 	}
 	
 	//Array of an array:
-	var ordered_options = [];
-	for (var i = 0; i < index_display_order_base.length; i++)
+	let ordered_options = [];
+	for (let i = 0; i < index_display_order_base.length; i++)
 		ordered_options[i] = [];
 		
 	
-	for (var x = 0; x <= 99; x++)
+	for (let x = 0; x <= 99; x++)
 	{
-		var explaination = explain_it_all[x];
-		var intro = "";
+		let explaination = explain_it_all[x];
+		let url = explain_urls[x];
+		if (url === null || url === undefined)
+			url = "";
+		let intro = "";
 		if (solutions[x] != undefined)
 			intro = "<strong>" + solutions[x] + "</strong>";
 		else if (adventures_until_solution[x] > 0 && adventures_until_solution[x] < 99)
@@ -205,21 +281,26 @@ function solveNumberology()
 		else
 			intro = "Unknown";
 		
-		var outro = explaination;
+		let outro = "";
+		if (url.length > 0)
+			outro += "<a href=\"" + url + "\">";
+		outro += explaination;
+		if (url.length > 0)
+			outro += "</a>";
 		outro += " ";
 		outro += "<span style=\"color:grey;font-size:0.8em;\">(" + x + ")</span>";
 		
-		var line = [intro, outro];
+		let line = [intro, outro];
 		
 		if (explaination === "Try Again")
 		{
 			//not worth showing?
 			continue;
 		}
-		var importance_index = 1;
+		let importance_index = 1;
 		
 		
-		for (var i = 0; i < index_display_order.length; i++)
+		for (let i = 0; i < index_display_order.length; i++)
 		{
 			if (index_display_order[i][x] != undefined)
 			{
@@ -232,18 +313,18 @@ function solveNumberology()
 	}
 	
 	//Linearise options, calculate linebreak indices:
-	var all_options = [];
-	var linebreak_indices = [];
-	for (var i = 0; i < ordered_options.length; i++)
+	let all_options = [];
+	let linebreak_indices = [];
+	for (let i = 0; i < ordered_options.length; i++)
 	{
-		for (var j = 0; j < ordered_options[i].length; j++)
+		for (let j = 0; j < ordered_options[i].length; j++)
 			all_options.push(ordered_options[i][j]);
 		if (ordered_options[i].length > 0 && i != ordered_options.length - 1)
 			linebreak_indices[all_options.length - 1] = true;
 	}
 		
 	//Format HTML:
-	var result_html = "";
+	let result_html = "";
 	result_html += "<hr>";
 	
 	result_html += "<div style=\"display:table;margin-left:auto; margin-right:auto;\">";
@@ -252,11 +333,11 @@ function solveNumberology()
 	result_html += "<div style=\"display:table-cell;font-weight:bold;padding-left:1em;text-decoration:underline;\">For this</div>";
 	result_html += "</div>";
 	
-	for (var i = 0; i < all_options.length; i++)
+	for (let i = 0; i < all_options.length; i++)
 	{
-		var option = all_options[i];
+		let option = all_options[i];
 		
-		var row_html = "";
+		let row_html = "";
 		row_html += "<div style=\"display:table-row;";
 		if (option[1] === "Try Again")
 		{
@@ -287,14 +368,14 @@ function solveNumberology()
 
 function pageLoaded()
 {
-	var focus_on_level = false;
+	let focus_on_level = false;
 	try
 	{
 		if (typeof localStorage !== "undefined")
 		{
 			//retrieve from cache:
-			var saved_ascensions = localStorage.getItem("ascensions");
-			var saved_moon_sign = localStorage.getItem("moon_sign");
+			let saved_ascensions = localStorage.getItem("ascensions");
+			let saved_moon_sign = localStorage.getItem("moon_sign");
 			if (saved_ascensions != undefined)
 			{
 				document.getElementById("ascensions").value = saved_ascensions;
